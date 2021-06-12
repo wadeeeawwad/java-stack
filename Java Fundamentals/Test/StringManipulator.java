8fb38d19d68d8af2498105bbcbbd7bfc32929984
wadeeawwad <wadee_awwad@hotmail.com>
2021-06-12 22:11:02 +0300

    after delete

--- Java Fundamentals/Test/StringManipulator.java
+++
@@ -1,9 +0,0 @@
-public class StringManipulator {
-    public Integer getIndexOrNull(String str, char letter) {
-    int idx = str.indexOf(letter, 0);
-    if( idx == -1) {
-        return null;
-    }
-    return idx;
-}
-}
