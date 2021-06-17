dabb7ef8669e0febf668f6a0823e57b161432449
wadeeawwad <wadee_awwad@hotmail.com>
2021-06-17 11:52:24 +0300

    counter

---
+++ Spring Fundamentals/coununter/counter/src/main/java/com/example/counter/CounterApplication.java
@@ -0,0 +1,12 @@
+package com.example.counter;
+import org.springframework.boot.SpringApplication;
+
+
+
+public class CounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CounterApplication.class, args);
+	}
+
+}
