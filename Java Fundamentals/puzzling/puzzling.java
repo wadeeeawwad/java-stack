c7614301c44e83d0a885869f94491479ad6fb571
wadeeawwad <wadee_awwad@hotmail.com>
2021-06-28 13:53:15 +0300

    puzzling

---
+++ Java Fundamentals/puzzling/puzzling.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+class puzzling{
+    public static void main(String [] args){
+        // int sum=0;
+        // ArrayList<Integer> numbers = new ArrayList<Integer>();
+        // ArrayList<Integer> tenormore=new ArrayList<Integer>();
+        // String newstring=new String("3,5,1,2,7,9,8,13,25,32");
+        // for (String num:newstring.split(",")){
+        //     numbers.add(Integer.parseInt(num));
+        // }
+        // for (Integer num : numbers ){
+        //     sum+=num;
+        //     if(num>10){
+        //         tenormore.add(num);
+        //     }
+        // }   
+
+
+        // ArrayList<String> names=new ArrayList<String>();
+        // ArrayList<String> morethan5names=new ArrayList<String>();
+        // String newString=new String("Nancy,Jinichi,Fujibayashi,Momochi,Ishikawa");
+        // for(String name:newString.split(",")){
+        //     names.add(name);
+        // }
+        // Collections.shuffle(names);
+        // System.out.println(names);
+        // for(String name:names){
+        //     if(name.length()>5){
+        //         morethan5names.add(name);
+        //     }
+        // }
+        // System.out.println(morethan5names);
+        
+        // ArrayList<String> chars=new ArrayList<String>();
+        // String letters=new String("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z");
+        // for (String letter:letters.split(",")){
+        //     chars.add(letter);
+        // }
+        // Collections.shuffle(chars);
+        // System.out.println(chars.get(chars.size()-1));
+        // System.out.println(chars.get(0));
+        // if(chars.get(0).charAt(0)=='a' || chars.get(0).charAt(0)=='i' || chars.get(0).charAt(0)=='o' || chars.get(0).charAt(0)=='e' || chars.get(0).charAt(0)=='u'){
+        //     System.out.println("omg the first letter is a voul");
+        // }
+
+
+        // ArrayList<Integer> randoms=new ArrayList<Integer>();
+        // Random r = new Random();
+        // for(int i =0 ;i <10;i++){
+        //     randoms.add(55+r.nextInt(45));
+        // }
+        // System.out.println(randoms);\
+
+        
+        // ArrayList<String> randoms=new ArrayList<String>();
+        // for (int i =0;i<10;i++){
+        //     randoms.add(randomstring());
+        // }
+        // System.out.println(randoms);
+
+
+
+
+    }
+    // static String randomstring(){
+    //     Random r = new Random();
+    //     String world="";
+    //     char[] random=new char[5];
+    //     for (int i =0 ;i <5;i++){
+    //         char c = (char)(r.nextInt(26) + 'a');
+    //         world=world+c;
+    //     }
+    //     return world;
+    // }
+}
